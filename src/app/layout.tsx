29768fc838fadbf268f8c7b491c15df837c3a227
Lyndon Baylin Jr. <lyndon@syntacticsinc.com>
2025-03-17 01:54:39 +0800

    perf: initialize font helper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 
 import { ThemeProvider } from 'next-themes';
 
 import ReactQueryProvider from '@/providers/react-query-provider';
 
+import { geistMono, geistSans } from '@/utils/fonts';
+
 import './globals.css';
-
-const geistSans = Geist({ variable: '--font-geist-sans', subsets: ['latin'] });
-
-const geistMono = Geist_Mono({ variable: '--font-geist-mono', subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Next Boilerplate',
